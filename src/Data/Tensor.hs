f888492fab67e7133904490f71a32242e9f89456
ocramz <zocca.marco@gmail.com>
2018-01-23 05:50:58 +0100

    changed representation slighlty

--- src/Data/Tensor.hs
+++ src/Data/Tensor.hs
@@ -1,3 +1,4 @@
+{-# language GADTs #-}
 module Data.Tensor where
 
 import qualified Data.Vector.Unboxed as V
@@ -5,43 +6,60 @@ import qualified Data.Vector.Unboxed as V
 -- import Data.Word (Word32, Word64)
 
 
-
-
 -- * Dimension metadata
 
--- | To define a /dense/ dimension we only need its scalar size
-data DMDDense i = DMDDense { dmdSize :: i } deriving (Eq, Show)
+-- | To define a /dense/ dimension we only need the dimensionality parameter
+data DMDDense i = DMDDense { dDim :: Int } deriving (Eq, Show)
 
--- | To define a /sparse/ dimension we need both a position and an index array
+-- | To define a /sparse/ dimension we need a cumulative array, an index array and a dimensionality parameter
 data DMDSparse i = DMDSparse {
-      dmdPos :: V.Vector i
-    , dmdIdx :: V.Vector i }
+      sCml :: V.Vector i
+    , sIdx :: V.Vector i
+    , sDim :: Int }
   deriving (Eq, Show)
-
 
 -- | A tensor dimension can be either dense or sparse.
 --
 -- Example: the CSR format is /dense/ in the first index (rows) and /sparse/ in the second index (columns)
 newtype DMD i = DMD (Either (DMDDense i) (DMDSparse i)) deriving (Eq, Show)
 
--- | Tensor data entries are always stored as one single array, no matter the order and sparsity
-newtype TensorData a = TD { _tensorData :: V.Vector a } deriving (Eq, Show)
+-- | Tensor data entries are stored as one single array
+data Tensor i a = Tensor {
+    tensorData :: V.Vector a
+  , tensorIxs :: [DMD i]
+                         } deriving (Eq, Show)
+
+dim :: DMD i -> Int
+dim (DMD ed) = either dDim sDim ed
+
+dims :: Tensor i a -> [Int]
+dims t = dim <$> tensorIxs t
 
 
 
 
 
-data Expr a =
-    Const a
-  | Expr a :+: Expr a
-  | Expr a :*: Expr a
-  deriving (Eq, Show)
+data Expr a where
+  Const :: a -> Expr a 
+  -- ^ Sum two expressions
+  (:+:) :: Expr a -> Expr a -> Expr a
+  -- ^ Reduce over one or more indices
+  (:*:) :: Expr a -> Expr a -> Expr a
 
 
-eval :: Num t => Expr t -> t
-eval (Const x) = x
-eval (a :+: b) = eval a + eval b
-eval (a :*: b) = eval a * eval b
+
+
+-- data Expr a =
+--     Const a
+--   | Expr a :+: Expr a
+--   | Expr a :*: Expr a
+--   deriving (Eq, Show)
+
+
+-- eval :: Num t => Expr t -> t
+-- eval (Const x) = x
+-- eval (a :+: b) = eval a + eval b
+-- eval (a :*: b) = eval a * eval b
 
 
 
