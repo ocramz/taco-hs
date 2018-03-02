ff3d0d81148e3ddbbfd0e3dcd8b65392ce03d4da
ocramz <zocca.marco@gmail.com>
2018-03-02 16:36:42 +0100

    add Data.Tensor.Compiler.contract

--- src/Data/Tensor/Compiler.hs
+++ src/Data/Tensor/Compiler.hs
@@ -16,7 +16,7 @@ internally, tensor data is stored in /dense/ vectors
 
 -}
 module Data.Tensor.Compiler (
-    mkVar
+    mkVar, contract
     -- * Tensor types
   , Tensor(..), Sh(..), Dd(..), Sd(..)
     -- * Syntax
@@ -32,17 +32,16 @@ import Control.Exception (Exception(..))
 import Control.Applicative (liftA2, (<|>))
 
 import Data.Tensor (Tensor(..), Sh(..), Dd(..), Sd(..), tshape, tdata, nnz, rank, dim)
-import Data.Tensor.Compiler.PHOAS (Phoas(..), let_, let2_, var, lift1, lift2)
+import Data.Tensor.Compiler.PHOAS (Phoas(..), let_, let2_, var, lift1, lift2, eval)
 
 
 
 
 
--- contract2 (T sh1 d1) (T sh2 d2) = undefined
 
--- | Inject a 'Tensor' constan into a 'Var', while ensuring that all the contraction indices are compatible with those of the tensor.
+-- | Inject a 'Tensor' constant into a 'Var', while ensuring that all the contraction indices are compatible with those of the tensor.
 --
--- Throws exceptions if any index is nonnegative or 
+-- Throws exceptions if any index is nonnegative or too large for the shape of the given tensor
 mkVar :: MonadThrow m => [Int] -> Tensor i a -> m (Phoas (Tensor i a))
 mkVar [] t = pure $ var t
 mkVar (i:is) t
@@ -51,6 +50,19 @@ mkVar (i:is) t
   | i > rank t - 1 =
     throwM $ IncompatShape "Index exceeds is incompatible with the tensor rank"
   | otherwise = mkVar is t
+
+
+-- | Tensor contraction
+contract :: MonadThrow m =>
+                  [Int]         -- ^ Contraction indices
+                  -> Tensor i a 
+                  -> Tensor i a
+                  -> (Tensor i a -> Tensor i a -> Phoas (Tensor i a))  -- ^ Contraction function
+                  -> m (Phoas (Tensor i a))
+contract ixs t1 t2 f = do
+  t1p <- mkVar ixs t1
+  t2p <- mkVar ixs t2
+  pure $ let2_ t1p t2p f
 
 
 -- | Exceptions
