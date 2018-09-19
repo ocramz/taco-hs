41c83f5f5ef9c7f0a45b3aaa396174ce7c8d98ad
ocramz <zocca.marco@gmail.com>
2018-09-19 18:13:09 +0200

    how does the iteration graph look like?

--- src/Data/Tensor/Internal/MGraph.hs
+++ src/Data/Tensor/Internal/MGraph.hs
@@ -4,8 +4,37 @@ MGraph is a multigraph (i.e. a graph in which each pair of nodes could be connec
 module Data.Tensor.Internal.MGraph where
 
 import qualified Data.Map.Strict as M
+import qualified Data.List.NonEmpty as NE
+
+
+newtype MGraph l a = MGraph (M.Map l (NE.NonEmpty a)) deriving (Eq, Show)
 
 
 
+{-
+node :: a
+arc :: (a, a)
+-}
 
-newtype MGraph l a = MGraph (M.Map l a) deriving (Eq, Show)
+
+data Node f a = Node {
+    node :: a
+  , neighbors :: f (Node f a)
+                   } 
+
+singleL :: a -> Node [] a
+singleL x = Node x [] 
+
+-- -- | arrow from x to y
+-- connect :: Node a -> Node a -> Node a
+connect (Node x nx) y = Node x (y : nx) 
+
+-- -- path (Node x nx) = x : path (head nx)
+-- -- path (Node x []) = [x]
+
+-- path (Node x nx) = case
+--   nx of (nx0:_) -> x : path nx0
+--         [] -> [x]
+
+-- n0 = single 'z'
+-- n1 = single 'a'
