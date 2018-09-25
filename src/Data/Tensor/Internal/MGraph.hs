b7dab4441804c9139886305b852372a646fc88d3
ocramz <zocca.marco@gmail.com>
2018-09-25 10:06:25 +0200

    saving

--- src/Data/Tensor/Internal/MGraph.hs
+++ src/Data/Tensor/Internal/MGraph.hs
@@ -12,27 +12,56 @@ MGraph is a multigraph (i.e. a graph in which each pair of nodes could be connec
 -- {-# language GADTs #-}
 module Data.Tensor.Internal.MGraph where
 
+import Data.List (unfoldr)
+
 import qualified Data.Map.Strict as M
 import qualified Data.IntMap.Strict as IM
 import qualified Data.List.NonEmpty as NE
 
 
-newtype G a = G (IM.IntMap [a]) deriving (Eq, Show)
+
+data Op a = Reduce a a | Kron a a deriving (Eq, Show)
+
+inner uu vv = sum $ zipWith (*) uu vv
+
+inner' :: (Foldable f, Num a) => f (a, a) -> a
+inner' = foldr ins 0 where
+  ins (u, v) z = z + (u * v)
+
+-- | Kronecker product
+--
+-- >>> kron [2,3,1,2] [2,3,4,5]
+-- [[4,6,8,10],[6,9,12,15],[2,3,4,5],[4,6,8,10]]
+-- kron = unfoldr go [] where
+--   go ((uu, vv) : _)
+--     | null uu = Nothing
+    
+-- kron uvs = unfoldr go uvs where
+--   go ((uu, vv):_) | null uu = Nothing
+--                   | otherwise = Just ((head uu *) `map` vv, (tail uu, vv))
+-- kron :: (Functor f, Functor g, Num a) => f a -> g a -> f (g a)
+-- kron uu vv = fmap (\u -> (u *) <$> vv) uu
 
 
-fromList :: [a] -> G a
-fromList xs = G $ IM.fromList $ zip [0 .. ] xs' where xs' = map pure xs
+-- | 
 
-size :: G a -> Int
-size (G im) = IM.size im  
+-- newtype G a = G { unG :: IM.IntMap [a] } deriving (Eq, Show)
 
-mapKeys :: (IM.Key -> IM.Key) -> G a -> G a
-mapKeys f (G im) = G $ IM.mapKeys f im
+-- unionConcat :: G a -> G a -> G a
+-- unionConcat g1 g2 = G $ IM.unionWith (++) (unG g1) (unG g2)
 
+-- fromList :: [a] -> G a
+-- fromList xs = G $ IM.fromList $ zip [0 .. ] xs' where xs' = map pure xs
 
-pairAt i g1 g2 = undefined where
-  (sz1, sz2) = (size g1, size g2)
-  g2' = mapKeys (+ i) g2
+-- size :: G a -> Int
+-- size (G im) = IM.size im  
+
+-- mapKeys :: (IM.Key -> IM.Key) -> G a -> G a
+-- mapKeys f (G im) = G $ IM.mapKeys f im
+
+-- pairAt i g1 g2 = undefined where
+--   (sz1, sz2) = (size g1, size g2)
+--   g2' = mapKeys (+ i) g2
   
 
 
