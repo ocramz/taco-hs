cf426764016f38d33af76216d723d08fba2309c1
ocramz <zocca.marco@gmail.com>
2018-09-29 21:33:55 +0200

    fix csPtrV indexing

--- src/Data/Tensor/Internal/Vector.hs
+++ src/Data/Tensor/Internal/Vector.hs
@@ -20,16 +20,11 @@ import qualified Data.List.NonEmpty as NE
 
 newtype Row a = Row { unRow :: NE.NonEmpty a } deriving (Eq, Show)
 
-ix :: Int -> Row a -> Maybe a
-ix i (Row ne)
-  | i > NE.length ne || i < 0 = Nothing
-  | otherwise = Just $ ne NE.!! i
-
--- | NB : assumes the index is between 0 and (length - 1)
+-- | Unsafe : it assumes the index is between 0 and (length - 1)
 ixUnsafe :: Int -> Row a -> a
 ixUnsafe i (Row ne) = ne NE.!! i  
 
--- | NB : assumes the index is between 0 and (length - 1)
+-- | Unsafe : it assumes the index is between 0 and (length - 1)
 compareIx :: Ord a => Int -> Row a -> Row a -> Ordering
 compareIx i = comparing (ixUnsafe i)
 
@@ -45,6 +40,7 @@ ptrV ::
   -> V.Vector Int  -- ^ Pointer vector
 ptrV = csPtrV (==)
 
+
 -- | Given a number of rows(resp. columns) `n` and a _sorted_ Vector of Integers in increasing order (containing the column (resp. row) indices of nonzero entries), return the cumulative vector of nonzero entries of length `n + 1` (the "column (resp. row) pointer" of the CSR(CSC) format). NB: Fused count-and-accumulate
 -- E.g.:
 -- > csPtrV (==) 4 (V.fromList [1,1,2,3])
@@ -54,13 +50,16 @@ csPtrV eqf n xs = V.create createf where
   createf :: ST s (VM.MVector s Int)
   createf = do
     let c = 0
-    vm <- VM.new (n + 1)
+    vm <- VM.new n
     VM.write vm 0 0  -- write `0` at position 0
     let loop v ll i count | i == n = return ()
                           | otherwise = do
                               let lp = V.length $ V.takeWhile (`eqf` i) ll
                                   count' = count + lp
-                              VM.write v (i + 1) count'
+                              VM.write v i count'
                               loop v (V.drop lp ll) (succ i) count'
-    loop vm xs 0 c
+    loop vm xs 1 c
     return vm
+
+
+
